f10f8bbcd30c8416f213e2a75a59f5780dce7a04
Dilan <dilan.djrd@gmail.com>
2023-01-26 21:47:39 +0100

    Fin des exercices.
    Fin des exercices.
    
    Fin de l'exercice 4 et de l'exercice DOM

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ function Voiture(marque, modele, annee) {
     this.marque = marque;
     this.modele = modele;
     this.annee = annee;
+  }
   // Etape 2 : Ajouter une méthode "conduire" à l'objet "voiture"
-    this.conduire = function() {
+  Voiture.conduire = function() {
     console.log(`La voiture de marque ${this.marque} modèle ${this.modele} de l'année ${this.annee} roule sur la route.`);
-  }}
+  }
   
   // Etape 3 : Créer une instance de l'objet "voiture" en utilisant les propriétés "Toyota", "Camry" et "2020"
   let maVoiture = new Voiture("Toyota", "Camry", 2020);
@@ -24,9 +25,11 @@ function Voiture(marque, modele, annee) {
 let garage = [new Voiture("Toyota", "Camry", 2020), new Voiture("Tesla", "Model 3", 2017), new Voiture("Bugatti", "Chiron", 2016)];
 
 // Etape 2 : Ajouter une méthode "afficherVoitures" à l'objet "garage" qui affiche les propriétés de chaque voiture dans le tableau
-garage.afficherVoitures = () =>  garage.forEach(voiture => {
+  garage.afficherVoitures = function() { 
+    garage.forEach(voiture => {
     console.log(`Marque: ${voiture.marque}, Modèle: ${voiture.modele}, Année: ${voiture.annee}`);
   });
+}
 
 // Etape 3 : Appeler la méthode "afficherVoitures" sur l'objet "garage" pour afficher les propriétés de chaque voiture dans le tableau
 garage.afficherVoitures();
@@ -40,16 +43,44 @@ function Personne(nom, age) {
 // Etape 2 : Ajouter les propriétés "nom" et "âge" à l'objet créé par la fonction constructeur    
     this.nom = nom;
     this.age = age;
+  
 
 // Etape 3 : Ajouter une méthode "sePresenter" à l'objet créé par la fonction constructeur qui affiche un message "Bonjour, je m'appelle [nom] et j'ai [âge] ans."    
-    this.sePresenter = function() {
-        console.log(`Bonjour, je m'appelle ${nom} et j'ai ${age} ans.`);
-    }
+this.sePresenter = function()  {
+        console.log(`Bonjour, je m'appelle ${this.nom} et j'ai ${this.age} ans.`);
+  }
 }
+
 // Etape 4 : Utiliser la fonction constructeur pour créer une instance de l'objet "Personne" en utilisant les valeurs "John" et "30"
 let maPersonne = new Personne("John", 30);
 maPersonne.sePresenter();
   
 // Exercice 4 
 
-//
+//Etape 1 : Créer une fonction constructeur "Etudiant" qui hérite des propriétés et méthodes de la fonction constructeur "Personne"
+
+function Etudiant(nom, age, matricule) {
+    Personne.call(this, nom, age);
+    
+
+//Etape 2 : Ajouter une propriété "matricule" à l'objet créé par la fonction constructeur "Etudiant"  
+    this.matricule = matricule;
+
+
+//Etape 3 : Ajouter une méthode "etudier" à l'objet créé par la fonction constructeur "Etudiant" qui affiche un message "L'étudiant [nom] avec le matricule [matricule] étudie pour son examen."
+    this.etudier = function() {
+      console.log(`L'étudiant ${this.nom} avec le matricule ${this.matricule} étudie pour son examen.`);
+};
+}
+
+//Etape 4 : Utiliser la fonction constructeur "Etudiant" pour créer une instance de l'objet "Etudiant" en utilisant les valeurs "Jane" et "22" pour les propriétés "nom" et "âge", et "123456" pour la propriété "matricule"
+let firstEtudiant = new Etudiant("Jane", 22, "123456");
+
+//Etape 5 : Appeler la méthode "sePresenter" sur l'instance de l'objet "Etudiant" pour afficher le message "Bonjour, je m'appelle Jane et j'ai 22 ans."
+firstEtudiant.sePresenter();
+
+
+//Etape 6 : Appeler la méthode "etudier" sur l'instance de l'objet "Etudiant" pour afficher le message "L'étudiant Jane avec le matricule 123456 étudie pour son examen."
+firstEtudiant.etudier();
+
+  
---
+++ carrousel.js
@@ -0,0 +1,39 @@
+function Carrousel(images, elementId) {
+  this.images = images;
+  this.elementId = elementId;
+  this.indexActuel = 0;
+  this.afficherImage = function() {
+      let img = document.createElement("img");
+      img.src = this.images[this.indexActuel];
+      let element = document.getElementById(this.elementId);
+      element.innerHTML = "";
+      element.appendChild(img);
+  }
+  this.suivant = function() {
+      this.indexActuel++;
+      if (this.indexActuel === this.images.length) {
+        this.indexActuel = 0;
+      }
+      this.afficherImage();
+  }
+  this.precedent = function() {
+      this.indexActuel--;
+      if (this.indexActuel < 0) {
+        this.indexActuel = this.images.length - 1;
+      }
+      this.afficherImage();
+  }
+  this.startAutoPlay = function() {
+      setInterval(() => {
+          this.suivant();
+      }, 3000);
+  }
+}
+let mesImages = ["image1.jpg", "image2.jpg", "image3.jpg"];
+let monCarrousel = new Carrousel(mesImages, "carrousel");
+monCarrousel.afficherImage();
+let prevId = document.getElementById("prevId");
+let nextId = document.getElementById("nextId");
+prevId.addEventListener("click", ()=>monCarrousel.precedent());
+nextId.addEventListener("click", ()=>monCarrousel.suivant());
+monCarrousel.startAutoPlay();
